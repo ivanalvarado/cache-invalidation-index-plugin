1cf49a3c4a2ae4208c59242c42f1078b4c3fb80f
Ivan Alvarado <ivalvara21@gmail.com>
2025-02-09 00:34:55 -0800

    Print subprojects

--- cacheinvalidationindex/src/main/java/com/ivanalvarado/cacheinvalidationindex/CacheInvalidationIndexTask.kt
+++ cacheinvalidationindex/src/main/java/com/ivanalvarado/cacheinvalidationindex/CacheInvalidationIndexTask.kt
@@ -1,12 +1,27 @@
 package com.ivanalvarado.cacheinvalidationindex
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.tasks.TaskAction
 
 open class CacheInvalidationIndexTask : DefaultTask() {
 
     @TaskAction
     fun run() {
-        println("Hello from the CacheInvalidationIndexTask!")
+        val dependencyPairs = project.rootProject.dependencyPairs()
+        println("Dependency Pairs: $dependencyPairs")
+    }
+
+    private fun Project.dependencyPairs(): List<Triple<Project, Project, String>> {
+        return subprojects.flatMap { project ->
+            project.configurations
+                .filter { it.name == "implementation" }
+                .flatMap { configuration ->
+                    println("Configuration: ${configuration.name}")
+                    configuration.dependencies.filterIsInstance<ProjectDependency>()
+                        .map { Triple(project, it.dependencyProject, configuration.name) }
+                }
+        }
     }
 }
